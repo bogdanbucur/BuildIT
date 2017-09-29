4c89ab15ece4d16d8c666067e2bad338b4ecfec6
Bogdan Bucur <bogdan.bucur@udevoffice.ro>
2017-09-29 12:05:51 +0300

    Added the required files from config directory

---
+++ config/auth.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    'facebookAuth'      : {
+        'clientID'      : '1711833605791786',
+        'clientSecret'  : '0e14a8d575261a8d6c6a011abd1db7b7',
+        'callbackURL'   : 'http://bogdanb.udevoffice.com/auth/facebook/callback'
+    },
+
+    'githubAuth'        : {
+        'clientID'      : '62258a1c9de5cd30547f',
+        'clientSecret'  : 'e1e852a7d576a38bdefa221e7055d4bba90a825c',
+        'callbackURL'   : 'http://bogdanb.udevoffice.com/auth/github/callback'
+    },
+
+    'googleAuth'        : {
+        'clientID'      : '574785129868-dpg6161gta8o6ono09v4cd49mmkso911.apps.googleusercontent.com',
+        'clientSecret'  : 'a5Epaip3H6kUByudR1oM8QzF',
+        'callbackURL'   : 'http://bogdanb.udevoffice.com/auth/google/callback'
+    },
+
+    'instagram'         : {
+        'clientID'      : '5e009eb77a4e4b168513a473f47c7b11',
+        'clientSecret'  : '2845756bd7e442b0815651047ae1531e',
+        'callbackURL'   : 'http://bogdanb.udevoffice.com/auth/instagram/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://bogdanbucur:l0standdamnd@ds139942.mlab.com:39942/licentadb'
+
+};
